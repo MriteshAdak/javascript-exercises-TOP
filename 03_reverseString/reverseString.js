5ed69605433184b3b38c22b0aee285a4b5e2d944
MriteshAdak <72008709+MriteshAdak@users.noreply.github.com>
2024-06-25 23:15:53 +0530

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(aString)
+{
+    let reversedString = "";
 
+    for (let i = (aString.length - 1); i >= 0; i--) 
+    {
+        reversedString += aString[i];
+        console.log(i);
+    }
+
+    return reversedString; //alternatively return aString.split("").reverse().join("");
 };
 
 // Do not edit below this line
