a814d22e0bb04d837f2186a02b3b10a8fd52e03e
MriteshAdak <72008709+MriteshAdak@users.noreply.github.com>
2024-06-26 00:38:38 +0530

    Exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end)
+{
+    if (start < 0 || end < 0 || typeof(start) != 'number' || typeof(end) != 'number')
+        return "ERROR";
+    
+    let sum = 0;
 
+    let lower = start;
+    let higher = end;
+    
+    if (start > end)
+        {
+            higher = start;
+            lower = end;
+        }
+
+    for (i = lower; i <= higher; i++) sum += i;
+
+    return sum;
 };
 
 // Do not edit below this line
