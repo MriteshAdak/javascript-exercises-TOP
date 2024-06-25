7d9e8ee66878b50d866d1d2fcb8ca8c48ab24325
MriteshAdak <72008709+MriteshAdak@users.noreply.github.com>
2024-06-25 22:48:08 +0530

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(inputString, repeatTimes)
+{
+    if(repeatTimes < 0) return "ERROR"; //try catch is another alternative to if else?
+    else
+    {
+        repeatedString = "";
+        
+        for (let i = 0; i < repeatTimes; i++) //string.repeat() can be used instead
+            repeatedString += inputString;
 
+        return repeatedString;
+    }
 };
 
 // Do not edit below this line
