d066f06e1e6ca6f0c12a59c68add272d63fe0c58
MriteshAdak <72008709+MriteshAdak@users.noreply.github.com>
2024-06-26 00:00:45 +0530

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(anArray, ...optArgs)
+{
+    for (let arg of optArgs)
+    {
+        for (let i = anArray.length; i >= 0; i--) // Decrementing because the array length will change if splice is invoked 
+        {                                         // and subsequent elements will change indexes leading to
+            if (anArray[i] === arg)               // skipped elememts in the iteration
+                anArray.splice(i, 1);
+        }
+    }
+
+    return anArray;
 };
 
 // Do not edit below this line
