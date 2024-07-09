9ee4ccd1341b816d4ee80166788ad21e5f6548f6
MriteshAdak <72008709+MriteshAdak@users.noreply.github.com>
2024-07-09 22:55:26 +0530

    exercise completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    
+    if(index < 0)
+        return 'OOPS';
 
+    index = parseInt(index);
+    const series = createFibonacciSeq(index + 1);
+    
+    return series[index];
 };
+
+function createFibonacciSeq(length)
+{
+    let fibonacciSeries = [];
+    for (let i = 0; i < length; i++)
+    {
+        if (i == 0 || i == 1)
+        {
+            fibonacciSeries.push(i);
+            continue;
+        }
+
+        fibonacciSeries.push(fibonacciSeries[i - 1] + fibonacciSeries[i - 2]);
+    }
+
+    return fibonacciSeries;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
