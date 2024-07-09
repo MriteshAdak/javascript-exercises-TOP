1409c07dda72c496ab098d31eb2c091f21aea21b
MriteshAdak <72008709+MriteshAdak@users.noreply.github.com>
2024-07-09 21:34:26 +0530

    completed exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanStr = str.toLowerCase().split('').filter((char) => 
+        {
+            return ((char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) || (char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57));
+        }).join('');
 
+    const reverseStr = [...cleanStr].reverse().join('');
+    
+    return reverseStr === cleanStr;
 };
 
 // Do not edit below this line
