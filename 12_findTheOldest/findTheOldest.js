8fdd1a50df5e903191203383ccfa6c102308b400
MriteshAdak <72008709+MriteshAdak@users.noreply.github.com>
2024-07-10 00:37:11 +0530

    completed exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((person1, person2) => 
+    {
+        const presentDate = new Date();
+        const presentYear = presentDate.getFullYear();
+        let age1, age2;
+
+        if (Object.keys(person1).includes('yearOfDeath'))
+            {age1 = person1.yearOfDeath - person1.yearOfBirth;}
+        else
+            {age1 = presentYear - person1.yearOfBirth;}
+
+        if (Object.keys(person2).includes('yearOfDeath'))
+            {age2 = person2.yearOfDeath - person2.yearOfBirth;} 
+        else
+            {age2 = presentYear - person2.yearOfBirth;}
+
+        if (age2 >= age1)
+            return person2;
+        else
+            return person1;
+    });
 
 };
 
